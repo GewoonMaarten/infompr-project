643bc7ffd9cbcaa7f21c40db78d1280540e5580f
Maarten de Klerk <maarten@deklerk.dev>
2021-01-12 18:58:33 +0100

    Add more models

--- utils/image_model_factory.py
+++ utils/image_model_factory.py
@@ -1,7 +1,10 @@
 from enum import Enum, auto
 from tensorflow.keras.applications.inception_v3 import InceptionV3
-from tensorflow.keras.layers import Dense, GlobalAveragePooling2D
+from tensorflow.keras.applications.inception_resnet_v2 import InceptionResNetV2
+from tensorflow.keras.applications import EfficientNetB7
+from tensorflow.keras.layers import BatchNormalization, Dense, Dropout, Flatten, GlobalAveragePooling2D
 from tensorflow.keras.models import Model
+from pathlib import Path
 
 
 class ModelType(Enum):
@@ -10,13 +13,13 @@ class ModelType(Enum):
     EFFIECENTNET = auto()
 
 
-def build_model(model_type, n_labels):    
+def build_model(model_type, n_labels):
     if model_type == ModelType.INCEPTION:
         model = __build_inception(n_labels)
     elif model_type == ModelType.INCEPTIONRESNET:
         model = __build_inceptionresnet
     elif model_type == ModelType.EFFIECENTNET:
-        model = __build_effiecentnet()
+        model = __build_effiecentnet(n_labels)
     else:
         raise ValueError('Not a valid value for model')
 
@@ -30,21 +33,45 @@ def __build_inception(n_labels):
     x = base_model.output
     x = GlobalAveragePooling2D()(x)
     x = Dense(1024, activation='relu')(x)
+
     predictions = Dense(n_labels, activation='softmax')(x)
 
-    return Model(inputs=base_model.input, outputs=predictions)
+    return Model(inputs=base_model.input, outputs=predictions, name="Inception")
 
 
-def __build_inceptionresnet():
-    pass
+def __build_inceptionresnet(n_labels):
+    base_model = InceptionResNetV2(weights='imagenet', include_top=False)
+
+    x = base_model.output
+    x = Flatten()(x)
+    x = Dropout(0.5)(x)
+
+    predictions = Dense(n_labels, activation='softmax')(x)
+
+    return Model(inputs=base_model.input, outputs=predictions, name="Inception-ResNet")
 
 
-def __build_effiecentnet():
-    pass
+def __build_effiecentnet(n_labels):
+    weights_path = Path(Path().absolute(),
+                        'bin',
+                        'noisy-student',
+                        'efficientnetb7_notop.h5')
+
+    if not weights_path.exists:
+        raise Exception('Could not find weights file?!')
+
+    base_model = EfficientNetB7(weights=str(weights_path), include_top=False)
+
+    x = base_model.output
+    x = GlobalAveragePooling2D(name="avg_pool")(x)
+    x = BatchNormalization()(x)
+    x = Dropout(0.2, name="top_dropout")(x)
+
+    predictions = Dense(n_labels, activation="softmax")(x)
+
+    return Model(inputs=base_model.input, outputs=predictions, name="EfficientNet")
 
 
 def __compile(model, is_binary):
     loss = 'binary_crossentropy' if is_binary else 'categorical_crossentropy'
-    optimizer = 'adam' if is_binary else 'rmsprop'
-
-    model.compile(optimizer=optimizer, loss=loss, metrics=['accuracy'])
+    model.compile(optimizer='adam', loss=loss, metrics=['accuracy'])
