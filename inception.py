b6c50258a4ea622ecfcd1596db5b84a2b620c21b
Maarten de Klerk <maarten@deklerk.dev>
2021-01-15 10:08:14 +0100

    Refactor image model factory

--- inception.py
+++ inception.py
@@ -1,5 +1,5 @@
 from utils.data_sequence import FakedditSequence, ModeType
-from utils.image_model_factory import build_model, ModelType
+from utils.image_model_factory import ModelBuilder, ModelType
 from utils.config import (
     training_batch_size,
     training_epochs,
@@ -9,27 +9,28 @@ from utils.config import (
 n_labels = 2
 
 train_seq = FakedditSequence(
-    training_batch_size, 
-    img_size, 
-    mode=ModeType.TRAIN, 
+    training_batch_size,
+    img_size,
+    mode=ModeType.TRAIN,
     n_labels=n_labels)
 
 test_seq = FakedditSequence(
     training_batch_size,
-    img_size, 
+    img_size,
     mode=ModeType.TEST,
-     n_labels=n_labels)
-    
-validate_seq = FakedditSequence(
-    training_batch_size, 
-    img_size, 
-    mode=ModeType.VALIDATE, 
     n_labels=n_labels)
 
-model = build_model(ModelType.INCEPTION, n_labels=n_labels)
+validate_seq = FakedditSequence(
+    training_batch_size,
+    img_size,
+    mode=ModeType.VALIDATE,
+    n_labels=n_labels)
 
-history = model.fit(train_seq, validation_data=validate_seq, epochs=training_epochs)
-score = model.evaluate(test_seq)
+model_builder = ModelBuilder(ModelType.INCEPTION, n_labels=n_labels)
+model_builder.compile_for_transfer_learning()
+
+history = model_builder.model.fit(train_seq, validation_data=validate_seq, epochs=training_epochs)
+score = model_builder.model.evaluate(test_seq)
 
 print(f'Test loss: {score}')
 
--- utils/image_model_factory.py
+++ utils/image_model_factory.py
@@ -13,6 +13,7 @@ class ModelType(Enum):
     EFFIECENTNET = auto()
 
 
+<<<<<<< HEAD
 def build_model(model_type, n_labels, path_to_weights=None):
     if model_type == ModelType.INCEPTION:
         model = __build_inception(n_labels, path_to_weights)
@@ -87,3 +88,81 @@ def __compile(model, is_binary):
 
     loss = 'binary_crossentropy' if is_binary else 'categorical_crossentropy'
     model.compile(optimizer='adam', loss=loss, metrics=['accuracy'])
+=======
+class ModelBuilder():
+    def __init__(self, model_type, n_labels) -> None:
+        self.model_type = model_type
+        self.n_lables = n_labels
+        self.model = None
+        self.base_model = None
+        
+        if model_type == ModelType.INCEPTION:
+            self.__build_inception(n_labels)
+        elif model_type == ModelType.INCEPTIONRESNET:
+            self.__build_inceptionresnet(n_labels)
+        elif model_type == ModelType.EFFIECENTNET:
+            self.__build_effiecentnet(n_labels)
+        else:
+            raise ValueError('Not a valid value for model')
+
+
+    def __build_inception(self, n_labels):
+        self.base_model = InceptionV3(weights='imagenet', include_top=False)
+
+        x = self.base_model.output
+        x = GlobalAveragePooling2D()(x)
+        x = Dense(1024, activation='relu')(x)
+
+        predictions = Dense(n_labels, activation='softmax')(x)
+
+        self.model = Model(inputs=self.base_model.input,
+                           outputs=predictions, 
+                           name="Inception")
+
+    def __build_inceptionresnet(self, n_labels):
+        self.base_model = InceptionResNetV2(weights='imagenet', 
+                                            include_top=False)
+
+        x = self.base_model.output
+        x = Flatten()(x)
+        x = Dropout(0.5)(x)
+
+        predictions = Dense(n_labels, activation='softmax')(x)
+
+        self.model = Model(inputs=self.base_model.input,
+                           outputs=predictions, 
+                           name="Inception-ResNet")
+
+    def __build_effiecentnet(self, n_labels):
+        weights_path = Path(Path().absolute(),
+                            'bin',
+                            'noisy-student',
+                            'efficientnetb7_notop.h5')
+
+        if not weights_path.exists():
+            raise Exception('Could not find weights file?!')
+
+        self.base_model = EfficientNetB7(weights=str(weights_path), 
+                                         include_top=False)
+
+        x = self.base_model.output
+        x = GlobalAveragePooling2D(name="avg_pool")(x)
+        x = BatchNormalization()(x)
+        x = Dropout(0.2, name="top_dropout")(x)
+
+        predictions = Dense(n_labels, activation="softmax")(x)
+
+        self.model = Model(inputs=self.base_model.input,
+                           outputs=predictions, 
+                           name="EfficientNet")
+
+    def compile_for_transfer_learning(self):
+        for layer in self.base_model.layers:
+            layer.trainable = False
+
+        loss = 'binary_crossentropy' if self.n_lables == 2 else 'categorical_crossentropy'
+        self.model.compile(optimizer='adam', loss=loss, metrics=['accuracy'])
+
+    def compile_for_fine_tuning(self):
+        pass
+>>>>>>> 1e95774 (Refactor image model factory)
